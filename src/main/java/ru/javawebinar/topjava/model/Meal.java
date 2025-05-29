a119ea6a25befe736a2c5951ec5f6aa152f8be3a
lvpolozo <lvpolozo@mts.ru>
2025-05-29 11:08:24 +0300

    add lesson.md

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,21 +1,41 @@
 package ru.javawebinar.topjava.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.GET_BETWEEN,
+                query = "SELECT m FROM Meal m  " +
+                        "WHERE m.user.id=:userId " +
+                        "AND m.dateTime >=:startDateTime " +
+                        "AND m.dateTime < :endDateTime " +
+                        "ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+
+})
 @Entity
-@Table(name = "meal")
+@Table(name = "meal", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meal_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+    public static final String ALL_SORTED  = "Meal.getAll" ;
+    public static final String GET_BETWEEN = "Meal.getBetween";
+    public static final String DELETE = "Meal.delete";
+
     @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
     @Column(name = "description", nullable = false)
+    @NotNull
     private String description;
     @Column(name = "calories", nullable = false)
+    @NotNull
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
     private User user;
 
     public Meal() {
